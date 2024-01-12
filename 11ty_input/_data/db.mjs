33a5cc97fcf700791f069c726446397f4694c6ae
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-01-12 19:41:57 +0100

    fix gh build

--- 11ty_input/_data/db.mjs
+++ 11ty_input/_data/db.mjs
@@ -10,13 +10,11 @@ export default async function () {
   const ressources = await db.all('SELECT * FROM ressources')
   const books = await db.all('SELECT * FROM ressources WHERE kind="book"')
 
-  // const episodes = await db.all('SELECT * FROM episodes ORDER BY youtube_published_at DESC')
   const interviews = await db.all('SELECT * FROM episodes WHERE kind = "interview" ORDER BY youtube_published_at DESC')
   const readings = await db.all('SELECT * FROM episodes WHERE kind = "reading" ORDER BY youtube_published_at DESC')
   const specials = await db.all('SELECT * FROM episodes WHERE kind = "special" ORDER BY youtube_published_at DESC')
 
   await db.close()
-
 
   return { ressources, interviews, readings, specials, books }
 }
